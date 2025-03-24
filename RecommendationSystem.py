e6c9f2172d379144b1eea3bce80cacb2a4a7db03
Xreflextion <lillian.ye06@gmail.com>
2025-03-24 02:33:47 -0400

    recommendation system docstrings

--- RecommendationSystem.py
+++ RecommendationSystem.py
@@ -1,29 +1,39 @@
+"""
+This module contains the RecommendationSystem class, which generates song recommendations.
+"""
 import heapq
 
 from GenerateGraph import generate_song_graph
 from SongGraph import SongGraph
 
 
-class RecommendationSystem():
+class RecommendationSystem:
     """
-    Song recommendation system
+    Song recommendation system class to generate recommendations.
+
+    Instance Attributes:
+        - graph: SongGraph object that contains the song graph
     """
     graph: SongGraph
 
     def __init__(self):
         """
-        Initialize the class
+        Initialize the RecommendationSystem class and generate the song graph.
         """
         self.graph = generate_song_graph()
-    
+
     def generate_recommendation(self, orig_vertex_id: str, n: int) -> list[str]:
         """
-        Given the id of a song in the graph, generate n recommendations that are closest to the song
-        song name, artist, score
+        Given the id of a song in the graph, generate n song recommendations that
+        are similar to the song and return their vertex_ids in a list.
+
+        Preconditions:
+            - orig_vertex_id in self.graph._vertices
+            - n > 0
         """
         shortest_distance = {orig_vertex_id: 0}
-        
-        queue = [(0, orig_vertex_id)]
+
+        queue = [(0.0, orig_vertex_id)]
         visited = set()
         while queue and len(shortest_distance) + 1 < n:
             score, vertex_id = heapq.heappop(queue)
@@ -42,19 +52,19 @@ class RecommendationSystem():
         res.sort(key=lambda x: x[1])
         print(res)
         return list([x[0] for x in res[:n]])
-                    
-                        
-        
+
+
+
 if __name__ == '__main__':
     vertex_id = "5SuOikwiRyPMVoIQDJUgSV"
     sys = RecommendationSystem()
     recs = sys.generate_recommendation(vertex_id, 5)
 
     print(sys.graph.get_vertex_details(vertex_id))
-    
+
     for neighbour_id in sys.graph.get_neighbours(vertex_id):
         print(sys.graph.get_neighbours(vertex_id)[neighbour_id])
-    
+
     for rec_id in recs:
         print(sys.graph.get_vertex_details(rec_id)['name'])
         # print(sys.graph.get_vertex_details(rec_id))
