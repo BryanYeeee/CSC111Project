04e6ca55e4cd9fe6ab7d3f4c41360ffeef4f2cc2
BryanYeeee <haloday873@gmail.com>
2025-03-24 00:20:45 -0400

    generate graph

--- GenerateGraph.py
+++ GenerateGraph.py
@@ -11,39 +11,39 @@ DATASET_NAME_2 = "datasets/kaggle_spotify_songs_2.csv"
 
 TESTING_LIMIT = 550
 
-
 def generate_song_graph() -> SongGraph:
-    """Generate some stuffs
     """
-    g = SongGraph()
-    max = TESTING_LIMIT
+    Generate some stuffs
+    """
+    new_graph = SongGraph()
+    limit = TESTING_LIMIT
 
     with open(DATASET_NAME_1, 'r', encoding="utf-8") as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
-            g.add_vertex(row[1], 'song')
+            new_graph.add_vertex(row[1], row[4], 'song')
             for artist in row[2].split(";"):
-                artist_id = uuid.uuid4()
-                g.add_vertex(artist_id, 'artist')
-                g.add_edge(row[1], artist_id)
-            max = max-1
-            if max ==TESTING_LIMIT/2:
+                artist_id = str(uuid.uuid4())
+                new_graph.add_vertex(artist_id, artist, 'artist')
+                new_graph.add_edge(row[1], artist_id, 0)
+            limit = limit - 1
+            if limit == TESTING_LIMIT/2:
                 break
 
     with open(DATASET_NAME_2, 'r', encoding="utf-8") as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
-            g.add_vertex(row[0], 'song')
-            artist_id = uuid.uuid4()
-            g.add_vertex(artist_id, 'artist')
-            g.add_edge(row[0], artist_id)
-            max = max-1
-            if max ==0:
+            new_graph.add_vertex(row[0], row[1], 'song')
+            artist_id = str(uuid.uuid4())
+            new_graph.add_vertex(artist_id, row[2], 'artist')
+            new_graph.add_edge(row[0], artist_id, 0)
+            limit = limit - 1
+            if limit == 0:
                 break
 
-    return g
+    return new_graph
 
 
 if __name__ == '__main__':
