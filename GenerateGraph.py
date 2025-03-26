973af22f49e022bc4dabce39bed96ce5c87b173c
BryanYeeee <haloday873@gmail.com>
2025-03-25 20:46:51 -0400

    generate song list names

--- GenerateGraph.py
+++ GenerateGraph.py
@@ -12,10 +12,13 @@ TESTING_LIMIT = 550
 BASE_GENRES = {"pop", "rock", "techno", "metal", "house", "reggae", "songwriter"}
 SAME_GENRES = {"rap": "hip-hop", "r&b": "r-n-b"}
 
-def filter_genre(genre: str) -> str:  
+SEARCH_BAR_SPLITTER = "｜"  # may need this as well in the file wherever the dictionary/list is generated
+
+
+def filter_genre(genre: str) -> str:
     """
     Given a genre, filter it to a base or common genre if possible.
-    """  
+    """
     if genre in SAME_GENRES:
         return SAME_GENRES[genre]
     for base_genre in BASE_GENRES:
@@ -24,13 +27,14 @@ def filter_genre(genre: str) -> str:
     return genre
 
 
-def generate_song_graph() -> SongGraph:
+def generate_song_graph() -> (SongGraph, dict[str: str]):
     """
     Generates a SongGraph by reading two CSV datasets containing Spotify song data.
     Each song in the dataset is added as a vertex in the graph with its properties like
     genre, danceability, energy, tempo, artists, etc.
     """
     new_graph = SongGraph()
+    song_list_names = {}
     limit = TESTING_LIMIT
 
     with open(DATASET_NAME_1, 'r', encoding="utf-8") as file:
@@ -42,6 +46,7 @@ def generate_song_graph() -> SongGraph:
                 row[1], row[4], artists, row[8], row[9], row[10],
                 row[11], row[12], row[13], row[14], row[15], row[16],
                 row[17], row[18], filter_genre(row[20]))
+            song_list_names[row[4] + SEARCH_BAR_SPLITTER + row[2]] = row[1]
             limit = limit - 1
             if limit == TESTING_LIMIT / 2:
                 break
@@ -54,16 +59,19 @@ def generate_song_graph() -> SongGraph:
                 row[0], row[1], {row[2]}, row[11], row[12], row[13],
                 row[14], row[15], row[16], row[17], row[18], row[19],
                 row[20], row[21], filter_genre(row[9]))
+            song_list_names[row[1] + SEARCH_BAR_SPLITTER + row[2]] = row[0]
             limit = limit - 1
             if limit == 0:
                 break
-    return new_graph
+    return new_graph, song_list_names
 
 
 if __name__ == '__main__':
     # TODO: Comment out, used for testing
-    g = generate_song_graph()
+    g, songs = generate_song_graph()
     print(g.get_average_edges())
     print(len(g._vertices["5SuOikwiRyPMVoIQDJUgSV"].neighbours))
     print(len(g._vertices))
-    visualize_graph(g)
+    # visualize_graph(g)
+
+    print(songs)
--- RecommendationSystem.py
+++ RecommendationSystem.py
@@ -4,25 +4,26 @@ This module contains the RecommendationSystem class, which generates song recomm
 from GenerateGraph import generate_song_graph
 from SongGraph import SongGraph
 
+SEARCH_BAR_SPLITTER = "｜"  # may need this as well in the file wherever the dictionary/list is generated
 
-SEARCH_BAR_SPLITTER = " | " # may need this as well in the file wherever the dictionary/list is generated
+
 class RecommendationSystem:
     """
     Song recommendation system class to generate recommendations.
 
     Instance Attributes:
         - graph: SongGraph object that contains the song graph
+        - song_list_names: a dictionary mapping song names to its vertex id in graph
     """
     graph: SongGraph
-    song_list_names: list[str]
+    song_list_names: dir[str: str]
 
     def __init__(self):
         """
         Initialize the RecommendationSystem class and generate the song graph.
         """
-        self.graph = generate_song_graph() # ** Maybe have this return the song_list_names + a dictionary with vertex_id as the key
-        self.song_list_names = [f'Comedy{SEARCH_BAR_SPLITTER}Gen Hoshino']
-        
+        self.graph, self.song_list_names = generate_song_graph()
+
     def obtain_vertex_id(self, given_input: str) -> str:
         """
         Given a string formatted like "song_name<SEARCH_BAR_SPLITTER>artist_name", return the vertex_id of the song.
@@ -33,7 +34,7 @@ class RecommendationSystem:
 
     def generate_recommendations(self, given_input: str, n: int) -> list[str, str, str]:
         """
-        Given a song name and artist, return a list of n tuples where the tuple is formatted like 
+        Given a song name and artist, return a list of n tuples where the tuple is formatted like
         (song_name, artist_name, score).
         """
         vertex_id = self.obtain_vertex_id(given_input)
